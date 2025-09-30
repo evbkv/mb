8d831bd78f6585f1ad44a0288e416ac633d8595f
Evgenii Bykov <100200240+evbkv@users.noreply.github.com>
2025-09-30 12:59:06 +0300

    Update settings.php

--- mb/settings.php
+++ mb/settings.php
@@ -9,6 +9,7 @@ $AUTHOR_NAME = 'evbkv';
 $AUTHOR_PASS = '0000';
 $lang = ['en', 'en_EN'];
 $blogTitle = 'Blog';
+$blogDescription = '';
 $indexPosts = 5;
 $homePage = ['Home', 'https://github.com/evbkv'];
 $lastPosts = 5;
