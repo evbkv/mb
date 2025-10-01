7d2a713f9d3c462b840822122fad8b348a0eb1fa
Evgenii Bykov <100200240+evbkv@users.noreply.github.com>
2025-10-01 16:36:15 +0300

    Update index.php

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ $basePath = rtrim(dirname($scriptName), '/\\');
 if ($basePath==='/' || $basePath==='\\') $basePath='';
 $scheme = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? 'https' : 'http';
 $host = $_SERVER['HTTP_HOST'];
-$url = $scheme . '://' . $host . $basePath . '/' . $slug;
+$url = $scheme . '://' . $host . $basePath . '/';
 
 $root = __DIR__;
 $posts = [];
